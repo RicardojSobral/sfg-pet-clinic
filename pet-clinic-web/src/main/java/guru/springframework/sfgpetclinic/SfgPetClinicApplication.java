4a4aa651c2ec2e83470e2df98344bb891e4dae42
Ricardo <2160824@my.ipleiria.pt>
2021-01-02 19:13:10 +0000

    release:prepare

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,13 +1,13 @@
-package guru.sprinframework.sfgpetclinic;
+package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
 
 }
