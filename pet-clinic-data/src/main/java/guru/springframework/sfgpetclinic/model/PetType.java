b1a39be0e90a89c4d748f7f5f67e4a9a7f991b38
RicardojSobral <rjoaosobral@gmail.com>
2021-03-17 16:36:58 +0000

    add circleci

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "types")
+public class PetType extends BaseEntity{
+
+	@Column(name = "name")
+	private String name;
+	
+}
